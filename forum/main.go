328d856bb3d4f519145bb72c4ae8542be9a42827
Moleque <molecada@yandex.ru>
2018-10-14 14:44:31 +0300

    Firt version

--- forum/main.go
+++ forum/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Docker test!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Docker test!")
+	})
+
+	log.Println("Server started!")
+	http.ListenAndServe(":5000", mux)
 }

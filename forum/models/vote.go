fa9abfc3e055a091b77c298a2c9f5aeb081cbcc2
Moleque <molecada@yandex.ru>
2018-10-29 15:09:06 +0300

    Vote's functions added

--- forum/models/vote.go
+++ forum/models/vote.go
@@ -1,63 +1,97 @@
 package models
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
+	"tp_db/forum/database"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 type Vote struct {
-	Nickname string  `json:"nickname"`
-	Voice    float32 `json:"voice"`
+	Nickname string `json:"nickname"`
+	Voice    int32  `json:"voice"`
 }
 
 const createVote = `
-	INSERT INTO vote (thread_id, user_id, value)
-	VALUES ($1, $2, $3)`
+	INSERT INTO votes (thread_id, username, value)
+	VALUES ($1, $2, $3) RETURNING username, value`
+
+const selectVoice = `
+	SELECT value
+	FROM votes
+	WHERE thread_id = $1 AND username = $2`
+
+const updateVoice = `
+	UPDATE votes
+	SET value = value + $3
+	WHERE thread_id = $1 AND username = $2
+	RETURNING value`
+
+const updateVotes = `
+	UPDATE threads
+	SET votes = votes + $2
+	WHERE id = $1`
 
 const addVoteToThread = `
-`
+	UPDATE threads 
+	SET votes = votes + $2
+	WHERE id = $1
+	RETURNING id, slug, created, title, message, username, forum, votes`
 
 func ThreadVote(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	// 	thread := getThreadBySlugId(params.ByName("slug_or_id"))
+	thread := getThreadBySlugId(params.ByName("slug_or_id"))
 
-	// 	vote := &Vote{}
-	// 	if decode(r.Body, thread) != nil {
-	// 		w.WriteHeader(http.StatusInternalServerError)
-	// 	}
+	vote := &Vote{}
+	if decode(r.Body, vote) != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 
-	// 	//создание голоса пользователя
-	// 	err := database.DB.QueryRow(createVote, thread.Id, nickname, forum).Scan(&thread.Id, &thread.Forum, &thread.Created, &thread.Title, &thread.Message, &thread.Author, &thread.Forum, &thread.Votes)
-	// 	if err, ok := err.(*pq.Error); ok {
-	// 		if err.Code.Name() == "unique_violation" {
-	// 			// if database.DB.QueryRow(selectForum, forum.Slug).Scan(&forum.Slug, &forum.Title, &forum.User, &forum.Threads, &forum.Posts) == nil {
-	// 			// 	jsonForum, _ := json.Marshal(forum)
-	// 			// w.WriteHeader(http.StatusConflict)
-	// 			// 	w.Write(jsonForum)
-	// 			// return
-	// 			// }
-	// 		}
-	// 		w.WriteHeader(http.StatusBadGateway)
-	// 		return
-	// 	}
+	//проверка наличия голоса на данный момент
+	var value int32
+	// if err :=
+	database.DB.QueryRow(selectVoice, thread.Id, vote.Nickname).Scan(&value)
+	log.Println("vvv", value, vote.Voice)
+	// err != nil {
+	// 	log.Println("ddd1", err)
+	// 	w.WriteHeader(http.StatusBadGateway)
+	// 	return
+	// }
 
-	// 	//добавление голоса к ветке
-	// 	err := database.DB.QueryRow(createVote, thread.Slug, thread.Created, thread.Title, thread.Message, nickname, forum).Scan(&thread.Id, &thread.Forum, &thread.Created, &thread.Title, &thread.Message, &thread.Author, &thread.Forum, &thread.Votes)
-	// 	if err, ok := err.(*pq.Error); ok {
-	// 		if err.Code.Name() == "unique_violation" {
-	// 			// if database.DB.QueryRow(selectForum, forum.Slug).Scan(&forum.Slug, &forum.Title, &forum.User, &forum.Threads, &forum.Posts) == nil {
-	// 			// 	jsonForum, _ := json.Marshal(forum)
-	// 			// w.WriteHeader(http.StatusConflict)
-	// 			// 	w.Write(jsonForum)
-	// 			// return
-	// 			// }
-	// 		}
-	// 		w.WriteHeader(http.StatusBadGateway)
-	// 		return
-	// 	}
+	//ОПТИМИЗАЦИЯ: создание триггера
+	switch value {
+	case 0:
+		//создание голоса пользователя
+		if err := database.DB.QueryRow(createVote, thread.Id, vote.Nickname, vote.Voice).Scan(&vote.Nickname, &vote.Voice); err != nil {
+			log.Println("ddd2", err)
+			w.WriteHeader(http.StatusBadGateway)
+			return
+		}
+		//добавление голоса к ветке
+		if err := database.DB.QueryRow(addVoteToThread, thread.Id, vote.Voice).Scan(&thread.Id, &thread.Slug, &thread.Created, &thread.Title, &thread.Message, &thread.Author, &thread.Forum, &thread.Votes); err != nil {
+			log.Println("ddd3", err)
+			w.WriteHeader(http.StatusBadGateway)
+			return
+		}
+	case 1:
+		if vote.Voice < 0 {
+			log.Println("+", value, vote.Voice)
+			database.DB.QueryRow(updateVoice, thread.Id, vote.Nickname, -2).Scan(&vote.Voice)
+			database.DB.QueryRow(updateVotes, thread.Id, -2).Scan(&vote.Nickname, &vote.Voice)
+			database.DB.QueryRow(selectThreadById, thread.Id).Scan(&thread.Id, &thread.Slug, &thread.Created, &thread.Title, &thread.Message, &thread.Author, &thread.Forum, &thread.Votes)
+		}
+	case -1:
+		if vote.Voice > 0 {
+			log.Println("-", value, vote.Voice)
+			database.DB.QueryRow(updateVoice, thread.Id, vote.Nickname, 2).Scan(&vote.Nickname, &vote.Voice)
+			database.DB.QueryRow(updateVotes, thread.Id, 2).Scan(&vote.Nickname, &vote.Voice)
+			database.DB.QueryRow(selectThreadById, thread.Id).Scan(&thread.Id, &thread.Slug, &thread.Created, &thread.Title, &thread.Message, &thread.Author, &thread.Forum, &thread.Votes)
+		}
+	}
 
-	// 	jsonThread, _ := json.Marshal(thread)
+	jsonThread, _ := json.Marshal(thread)
 	w.WriteHeader(http.StatusOK)
-	// 	w.Write(jsonThread)
+	w.Write(jsonThread)
 }

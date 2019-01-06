77e6565359192d7f17e1e00092cdde9f3c74de2c
Moleque <molecada@yandex.ru>
2019-01-06 19:32:48 +0300

    Fix forums a little bit

--- forum/models/forum.go
+++ forum/models/forum.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"tp_db/forum/database"
@@ -49,11 +50,13 @@ func ForumCreate(w http.ResponseWriter, r *http.Request, params httprouter.Param
 
 	err := database.DB.QueryRow(createForum, forum.Slug, forum.Title, forum.User).Scan(&forum.Slug, &forum.Title, &forum.User, &forum.Threads, &forum.Posts)
 	if err != nil {
+		fmt.Println(err)
 		if err.Error() == "pq: нулевое значение в столбце \"username\" нарушает ограничение NOT NULL" || err.Error() == "pq: null value in column \"username\" violates not-null constraint" {
 			w.WriteHeader(http.StatusNotFound)
 			w.Write(conflict("Can't find user by nickname:" + forum.User))
 			return
 		}
+		// ОПТИМИЗАЦИЯ: при конфликте возвращать существующую запись
 		if err.Error() == "pq: повторяющееся значение ключа нарушает ограничение уникальности \"forums_slug_key\"" || err.Error() == "pq: duplicate key value violates unique constraint \"forums_slug_key\"" {
 			if database.DB.QueryRow(selectForum, forum.Slug).Scan(&forum.Slug, &forum.Title, &forum.User, &forum.Threads, &forum.Posts) == nil {
 				jsonForum, _ := json.Marshal(forum)
@@ -89,19 +92,10 @@ func ForumGetOne(w http.ResponseWriter, r *http.Request, params httprouter.Param
 
 func ForumGetThreads(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	forum := params.ByName("slug")
-
-	var count int
-	database.DB.QueryRow(countThread, forum).Scan(&count)
-	if count == 0 {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write(conflict("Can't find forum by slug:" + forum))
-		return
-	}
+	slug := params.ByName("slug")
 
 	query := paramsGetThreads(selectThread, r)
-
-	rows, err := database.DB.Query(query, forum)
+	rows, err := database.DB.Query(query, slug)
 	if err != nil {
 		w.WriteHeader(http.StatusBadGateway)
 		return
@@ -114,6 +108,17 @@ func ForumGetThreads(w http.ResponseWriter, r *http.Request, params httprouter.P
 		rows.Scan(&thread.Id, &thread.Slug, &thread.Created, &thread.Title, &thread.Message, &thread.Author, &thread.Forum, &thread.Votes)
 		threads = append(threads, thread)
 	}
+
+	if len(threads) == 0 {
+		//проверка,что форум существует
+		forum := &Forum{}
+		if database.DB.QueryRow(selectForum, slug).Scan(&forum.Slug, &forum.Title, &forum.User, &forum.Threads, &forum.Posts) != nil {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write(conflict("Can't find forum by slug:" + slug))
+			return
+		}
+	}
+
 	jsonThreads, _ := json.Marshal(threads)
 	w.WriteHeader(http.StatusOK)
 	w.Write(jsonThreads)
